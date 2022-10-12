a3a1926386a512aa730a4c0869b355fec2e66c50
MelyLaur <melissanoemi.laurent@free.fr>
2022-10-12 21:45:26 +0200

    Affichage Finit

--- code/libs/display.utils.h
+++ code/libs/display.utils.h
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <conio.h>
 #include <windows.h>
+#include <stdarg.h>
 
 void clear_console();
 void display_board(Board *board);
@@ -10,6 +11,60 @@ void display_choice_mode_game();
 void display_choice_map();
 void display_choice_server();
 
+void display_header();
+
+int display(int *cursor, int *selected_menu);
+
+void display_header() {
+    clear_console();
+    printf("\n===============> Bienvenue dans Casse Briques <===============\n\n\n\n\n\n\n\n\t");
+    system("pause");
+}
+
+int display(int *cursor, int *selected_menu) {
+
+    if(*selected_menu == 1){
+        *cursor = (*cursor == 4) ? 3 : *cursor;
+        display_start_menu(*cursor);
+    }else if(*selected_menu == 2){
+        *cursor = (*cursor == 5) ? 4 : *cursor;
+        display_choice_mode_game(*cursor);
+    } else if(*selected_menu==3){
+        *cursor = (*cursor == 5) ? 4 : *cursor;
+        display_choice_map(*cursor);
+    } else if(*selected_menu == 4){
+        *cursor = (*cursor == 4) ? 3 : *cursor;
+        display_choice_server(*cursor);
+    }
+    int event = getch();
+    switch (event) {
+        case 72:
+            *cursor-=1;
+            *cursor = (*cursor == 0) ? 1 : *cursor;
+            break;
+        case 80:
+            *cursor+=1;
+            break;
+        case 13:
+            if(*selected_menu == 1 && *cursor == 3){
+                return 0;
+            } else if((*selected_menu == 2 && *cursor == 4) || (*selected_menu==3 && *cursor == 4)){
+                *selected_menu -= 1;
+            }else if(*selected_menu == 4 && *cursor == 3 ){
+                *selected_menu = 2;
+            }
+            else if(*selected_menu == 2 && *cursor == 3){
+                *selected_menu = 4;
+            }
+            else {
+                *selected_menu += 1;
+            }
+            break;
+        default:
+            break;
+    }
+    return 1;
+}
 
 /**
  * @function : Affiche le menu principal
@@ -17,49 +72,13 @@ void display_choice_server();
  *             2. Changer fichier de configuration
  *             3. Quitter
  */
-void display_start_menu(){
-    int cursorY =1;
-    //int exit = 0;
-    system("cls");
-    printf("\n===============> Bienvenue dans Casse Briques <===============\n\n\n\n\n\n\n\n\t");
-    system("psause");
+void display_start_menu(int cursor) {
     clear_console();
-
-    while(1){
-        clear_console();
-
-        printf("______________________________\n\n");
-        printf("\tMENU PRINCIPAL");
-        printf("\n______________________________\n\n\n");
-        printf("[%c] %s \n\n[%c] %s \n\n[%c] %s", cursorY == 1 ? 'X' : ' ',"Jouer",cursorY == 2 ? 'X' : ' ', "Configuration", cursorY == 3 ? 'X' : ' ', "Quitter");
-
-        printf("\n");
-        switch (_getch()) {
-            case 122:
-                cursorY--;
-                if(cursorY == 0 ) cursorY =1;
-                break;
-            case 115:
-                cursorY ++;
-                if(cursorY == 4 ) cursorY =3;
-                break;
-            case 13 :
-                if(cursorY == 3){
-                    return;
-
-                }else if(cursorY == 2){
-                    //fichier de configuration
-                }
-                else {
-                    display_choice_mode_game();
-                }
-                break;
-            }
-
-        }
-
-
-
+    printf("______________________________\n\n");
+    printf("\tMENU PRINCIPAL");
+    printf("\n______________________________\n\n\n");
+    printf("[%c] %s \n\n[%c] %s \n\n[%c] %s", cursor == 1 ? 'X' : ' ', "Jouer", cursor == 2 ? 'X' : ' ',
+           "Configuration", cursor == 3 ? 'X' : ' ', "Quitter");
 }
 
 /**
@@ -107,37 +126,13 @@ void display_choice_mode_game(){
 /**
  * @function : Affiche les cartes
  */
-void display_choice_map() {
-
-    int cursorY = 1;
-    while (1) {
-        system("cls");
-        printf("_____________________\n\n");
-        printf("\tCARTES");
-        printf("\n_____________________\n\n\n");
-        printf("[%c] %s \n\n[%c] %s \n\n[%c] %s \n\n[%c] %s", cursorY == 1 ? 'X' : ' ',"",cursorY == 2 ? 'X' : ' ', "", cursorY == 3 ? 'X' : ' ', "", cursorY == 4 ? 'X' : ' ', "Retour");
-
-        switch (_getch()) {
-            case 122:
-                cursorY--;
-                if (cursorY == 0) cursorY = 1;
-                break;
-            case 115:
-                cursorY++;
-                if (cursorY == 5) cursorY = 4;
-                break;
-
-            case 13 :
-                if (cursorY == 4) {
-                    return;
-                    //Fonction nbre de joueur
-                    //break;
-                }
-
-        }
-
-
-    }
+void display_choice_map(int cursor) {
+    system("cls");
+    printf("_____________________\n\n");
+    printf("\tCARTES");
+    printf("\n_____________________\n\n\n");
+    printf("[%c] %s \n\n[%c] %s \n\n[%c] %s \n\n[%c] %s", cursor == 1 ? 'X' : ' ', "", cursor == 2 ? 'X' : ' ',
+     "", cursor == 3 ? 'X' : ' ', "", cursor == 4 ? 'X' : ' ', "Retour");
 
 }
 
@@ -146,36 +141,13 @@ void display_choice_map() {
  * @features : 1. Démarrer un serveur
  *             2. Rejoindre un serveur
  */
-void display_choice_server(){
-
-    int cursorY = 1;
-    while (1) {
+void display_choice_server(int cursor) {
         system("cls");
         printf("________________________\n\n");
         printf("\tEN LIGNE");
         printf("\n________________________\n\n\n");
-        printf("[%c] %s \n\n[%c] %s \n\n[%c] %s ", cursorY == 1 ? 'X' : ' ',"Creer un serveur",cursorY == 2 ? 'X' : ' ', "Demarrer un serveur", cursorY == 3 ? 'X' : ' ', "Retour");
-
-
-        switch (_getch()) {
-            case 122:
-                cursorY--;
-                if (cursorY == 0) cursorY = 1;
-                break;
-            case 115:
-                cursorY++;
-                if (cursorY == 4) cursorY = 3;
-                break;
-
-            case 13 :
-                if (cursorY == 3) {
-                    return;
-                    //Fonction nbre de joueur
-                    //break;
-                }
-        }
-
-    }
+        printf("[%c] %s \n\n[%c] %s \n\n[%c] %s ", cursor == 1 ? 'X' : ' ', "Creer un serveur",
+               cursor == 2 ? 'X' : ' ', "Demarrer un serveur", cursor == 3 ? 'X' : ' ', "Retour");
 
 }
 
