4e925a160d750817cc518b91cbe7a7f2c7746f91
MelyLaur <melissanoemi.laurent@free.fr>
2022-10-19 22:58:55 +0200

    Création de bots

--- code/libs/game.utils.h
+++ code/libs/game.utils.h
@@ -1,75 +1,121 @@
-static char *LANGUAGE_DIR = "../configs/LANGUAGES/";
-static char *LANGUAGE_ATTRIBUTES[] = {
-        "EXIT",
-        "START",
-        "OPTIONS",
-        NULL
-};
+Board *generate_board();
 
+void run_game(Board *board);
+void run_menu(Board *board);
+void get_players();
+void menu_events(Board *board, int event);
 
-typedef struct {
-    char *exit;
-    char *start;
-    char *options;
-} Language;
+void run_game(Board *board) {
+    printf("RUN GAME");
+}
 
-typedef struct {
-    unsigned short port;
-    unsigned short language;
-} Config;
+void run_menu(Board *board) {
+    int event = getch();
+    menu_events(board, event);
 
-typedef struct {
-    unsigned short id;
-    unsigned short rows;
-    unsigned short columns;
+    display_menus(board);
 
-    unsigned char **body;
+    if(board->selected_menu == IN_PLAYERS) {
+        //board->players = get_players();
+    } else if(board->selected_menu == IN_MAPS) {
+//        board->selected_maps = get_selected_maps();
+    }
+}
 
-    unsigned short player_max;
-    unsigned short bomb_max;
+void menu_events(Board *board, int event) {
+    switch (event) {
+        case CROSS_TOP:
+        case KEY_z:
+            if (board->selected_choice - 1 >= 0) {
+                board->selected_choice -= 1;
+            }
+            break;
+        case CROSS_BOTTOM:
+        case KEY_s:
+            board->selected_choice += 1;
+            break;
+        case KEY_ENTER:
+            if(board->selected_menu == 1 && board->selected_choice == 2){
+                board->selected_menu = 0;
+            } else if ((board->selected_menu == 2 && board->selected_choice == 3) ||
+                       (board->selected_menu == 3 && board->selected_choice == 3)) {
+                board->selected_menu -= 1;
+            } else if (board->selected_menu == 4 && board->selected_choice == 2) {
+                board->selected_menu = 2;
+            } else if (board->selected_menu == 2 && board->selected_choice == 2) {
+                board->selected_menu = 4;
+            } else {
+                if (board->selected_menu + 1 < 5) {
+                    board->selected_menu += 1;
+                }
+            }
+            break;
+        default:
+            break;
+    }
+}
 
-    unsigned short nb_map;
-} Map;
+Board *generate_board() {
 
-typedef struct {
-    char id;
-    int range;
-    short bomb_kick;
-} Bomb;
+    Board *board = malloc(sizeof(Board));
+    Config *config = malloc(sizeof(Config));
 
-typedef struct {
-    char id;
-    int duration;
-    short rare;
-    short is_used;
-} Item;
+    config->language = malloc(sizeof(char) * 3);
+    config->language = "FR";
 
-typedef struct {
-    char *name;
-    char id;
-    char *color;
-    unsigned short direction;
+    board->config = config;
+    board->lang = get_lang(config->language);
+    board->winner = malloc(sizeof(Player));
+    board->players_turn = malloc(sizeof(Player));
 
-    unsigned short score;
-    unsigned short heart;
-    unsigned short nb_bomb;
-    Bomb *bomb;
-    Item **item;
+    //    board->maps = get_maps();
+    //    board->nb_map = get_nb_map(board->maps);
 
-    unsigned int placed_bomb;
-    unsigned short is_bot;
-    unsigned short selected_map;
-} Player;;
+    //    unsigned short nb_map;          //le nombre de carte
+    //    unsigned short nb_player;       //le nombre de joueur
 
-typedef struct {
-    Map **map;
-    Player **players;
-    Config *config;
-    Language *lang;
+    board->bo = 3;
+    board->selected_menu = 1;
+    board->selected_choice = 0;
+    board->selected_map = 0;
 
-    Player *winner;
-    Player *players_turn;
+    return board;
+}
 
-    unsigned short bo;
-    unsigned short cursor;
-} Board;
+Player *create_bot(Player *bot, int *index){
+    int j = rand()%10;
+    j = rand()%10 + 1;
+    char *bot_name[10] = {"Bob","Fox","Mewtwo","Ritcher","Rob","Joker","Bot","Bob","Toto","Test"};
+    //Melanger le tab
+    bot->name = bot_name[*index];
+    bot->is_bot = 1;
+    return bot;
+}
+
+/**
+ * @features : create player
+ * */
+Player *create_player(int *nb_added_player, int *nb_real_player,int *index){
+    Player *player = malloc(sizeof(Player));
+    player->name = malloc(sizeof (char));
+    player->id = *index+48;
+    player->score = 0;
+    player->heart = 1;
+    if(*nb_added_player != *nb_real_player){
+        printf("Entrez votre pseudo : ");
+        scanf("%s", player->name);
+        player->is_bot = 0;
+        *nb_added_player += 1;
+        return player;
+    }else {
+        create_bot(player,index);
+        return player;
+    }
+}
+
+/**
+ * @features : add player into player's tab
+ * */
+void push(Board *dest, Player *source, int index){
+    dest->players[index] = source;
+}
