83e00188611f16f854e4762ecf7824ad4e9ddf2c
Corentin <corentin.lechene.pro@gmail.com>
2022-11-11 16:05:11 +0100

    abandon

--- code/main.c
+++ code/main.c
@@ -14,100 +14,101 @@
 
 
 #define DEFAULT_BUFLEN 512
-#define PORT 27015
+#define DEFAULT_PORT "27015"
 
 int main() {
     int c;
     scanf("%d", &c);
     if(c == 0) {
-        SOCKET ClientSocket = INVALID_SOCKET;
-        
         WSADATA WSAData;
-        SOCKET sock;
-        SOCKADDR_IN sin;
-        SOCKADDR_IN csin;
         WSAStartup(MAKEWORD(2,0), &WSAData);
-        sock = socket(AF_INET, SOCK_STREAM, 0);
-        sin.sin_addr.s_addr = INADDR_ANY;
-        sin.sin_family = AF_INET;
-        sin.sin_port = htons(PORT);
         
-        bind(sock, (SOCKADDR *)&sin, sizeof(sin));
-        listen(sock, 0);
+        int iResult;
+        struct addrinfo *result = NULL, *ptr = NULL, hints;
+        SOCKET ListenSocket = INVALID_SOCKET;
+        SOCKET ClientSocket = INVALID_SOCKET;
+
+        ZeroMemory(&hints, sizeof (hints));
+        hints.ai_family = AF_INET;
+        hints.ai_socktype = SOCK_STREAM;
+        hints.ai_protocol = IPPROTO_TCP;
+        hints.ai_flags = AI_PASSIVE;
+
+        // Resolve the local address and port to be used by the server
+        iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+        if (iResult != 0) {
+            printf("getaddrinfo failed: %d\n", iResult);
+            WSACleanup();
+            return 1;
+        }
+
+        // Create a SOCKET for the server to listen for client connections
+        ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+
+        if (ListenSocket == INVALID_SOCKET) {
+            printf("Error at socket(): %ld\n", WSAGetLastError());
+            freeaddrinfo(result);
+            WSACleanup();
+            return 1;
+        }
+        // Setup the TCP listening socket
+        iResult = bind( ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+        if (iResult == SOCKET_ERROR) {
+            printf("bind failed with error: %d\n", WSAGetLastError());
+            freeaddrinfo(result);
+            closesocket(ListenSocket);
+            WSACleanup();
+            return 1;
+        }
+
+        if ( listen( ListenSocket, SOMAXCONN ) == SOCKET_ERROR ) {
+            printf( "Listen failed with error: %ld\n", WSAGetLastError() );
+            closesocket(ListenSocket);
+            WSACleanup();
+            return 1;
+        }
+
+        ClientSocket = accept(ListenSocket, NULL, NULL);
+        if (ClientSocket == INVALID_SOCKET) {
+            printf("accept failed: %d\n", WSAGetLastError());
+            closesocket(ListenSocket);
+            WSACleanup();
+            return 1;
+        }
         
-
-        // Accept a client socket
-        int sinsize = sizeof(csin);
-        ClientSocket = accept(sock, (SOCKADDR *)&csin, &sinsize);
         
-        char recvbuf[DEFAULT_BUFLEN];
-        int iResult, iSendResult;
-        int recvbuflen = DEFAULT_BUFLEN;
-
-        // Receive until the peer shuts down the connection
-        do {
-
-            iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
-            if (iResult > 0) {
-                printf("Bytes received: %d\n", iResult);
-                recvbuf[iResult] = '\0';
-
-                // Echo the buffer back to the sender
-                iSendResult = send(ClientSocket, recvbuf, iResult, 0);
-                if (iSendResult == SOCKET_ERROR) {
-                    printf("send failed: %d\n", WSAGetLastError());
-                    closesocket(ClientSocket);
-                    WSACleanup();
-                    return 1;
-                }
-                printf("Bytes sent: %d (%s)\n", iSendResult, recvbuf);
-                
-                if(strcmp(recvbuf, "end") == 0) {
-                    closesocket(ClientSocket);
-                    WSACleanup();
-                    return 1;
-                }
-            } else if (iResult == 0)
-                printf("Connection closing...\n");
-            else {
-                printf("recv failed: %d\n", WSAGetLastError());
-                closesocket(ClientSocket);
-                WSACleanup();
-                return 1;
-            }
-
-        } while (iResult > 0);
-        closesocket(sock);
+        closesocket(ClientSocket);
+        closesocket(ListenSocket);
         WSACleanup();
         return 0;
     } else {
-        WSADATA WSAData;
-        SOCKET sock;
-        SOCKADDR_IN sin;
-        char buffer[255];
-        char sendBuffer[255];
-        WSAStartup(MAKEWORD(2,0), &WSAData);
-        /* Tout est configuré pour se connecter sur IRC, haarlem, Undernet. */
-        sock = socket(AF_INET, SOCK_STREAM, 0);
-        sin.sin_addr.s_addr = inet_addr("192.168.0.18");
-        sin.sin_family = AF_INET;
-        sin.sin_port = htons(PORT);
-        connect(sock, (SOCKADDR *)&sin, sizeof(sin));
-
-        while(1) {
-            fflush(stdin);
-            scanf("%s", sendBuffer);
-            
-            send(sock, sendBuffer, (int)strlen(sendBuffer), 0);
-            recv(sock, buffer, sizeof(buffer), 0);
-            printf("[%s]", buffer);
-            if(strcmp(buffer, "end") == 0) {
-                closesocket(sock);
-                break;
-            }
-        }
-
-        WSACleanup();
+//        WSADATA WSAData;
+//        SOCKET sock;
+//        SOCKADDR_IN sin;
+//        char buffer[255];
+//        char sendBuffer[255];
+//        WSAStartup(MAKEWORD(2,0), &WSAData);
+//        /* Tout est configuré pour se connecter sur IRC, haarlem, Undernet. */
+//        sock = socket(AF_INET, SOCK_STREAM, 0);
+//        sin.sin_addr.s_addr = inet_addr("192.168.0.18");
+//        sin.sin_family = AF_INET;
+//        sin.sin_port = htons(PORT);
+//        connect(sock, (SOCKADDR *)&sin, sizeof(sin));
+//
+//        while(1) {
+//            fflush(stdin);
+//            scanf("%s", sendBuffer);
+//            
+//            send(sock, sendBuffer, (int)strlen(sendBuffer), 0);
+//            recv(sock, buffer, sizeof(buffer), 0);
+//            printf("[%s]", buffer);
+//            if(strcmp(buffer, "end") == 0) {
+//                closesocket(sock);
+//                break;
+//            }
+//        }
+//
+//        WSACleanup();
         return 0;
     }
 }
